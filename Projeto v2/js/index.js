0100350eff8c3c8533a0a4397c6d6c83363f3416
Derick Santana <derick.saann@gmail.com>
2021-04-14 20:20:39 -0300

    Add files via upload

---
+++ Projeto v2/js/index.js
@@ -0,0 +1,27 @@
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
+
+// NOME //
+
+function enter() {
+  span = document.querySelector("div#texto p")
+  div = document.querySelector("div.cor")
+
+  div.style.visibility = 'visible'
+}
+
+// SOBRE [Discord] //
+
+function discC() {
+  li = document.querySelector("li#discord");
+
+  li.innerHTML = 'banguelah com foto diferenciada#5405'
+}
+
+function discO() {
+  li = document.querySelector("li#discord");
+
+  li.innerHTML = 'Discord'
+}
